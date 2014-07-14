aec5dda0c5c186eda5876e96ec0b9504b7748ed8
chaluemwut_noyunsan <chaluemwut@gmail.com>
2014-07-14 23:06:05 +0700

    call all work

--- comparative.py
+++ comparative.py
@@ -1,6 +1,30 @@
 from wordsegmentation import WordSegmentation
 
+# spacial char 
+# <minus> -
+# <space>
+# <left_parenthesis> (
+# <right_parenthesis> )
+# <equal> =
+# new line \\
+# <quotation> "
+
 class MainCompare:
+	
+	def convert_result(self, next_data):
+		if next_data == '<space>/PUNC\n':
+			return ' '
+		elif next_data == '<minus>/PUNC\n':
+			return '-'
+		elif next_data == '<left_parenthesis>/PUNC\n':
+			return '('
+		elif next_data == '<right_parenthesis>/PUNC\n':
+			return ')'
+		elif next_data == '<quotation>/PUNC\n':
+			return '"'
+		else :
+			return next_data[:next_data.index("/")]
+		
 	
 	def process_data(self):
 		data = []
@@ -10,6 +34,7 @@ class MainCompare:
 		counter = 0
 		wordseg = WordSegmentation()
 		for orchid_data in data:
+			print orchid_data
 			first_char, sec_char, line_data = orchid_data[:1], orchid_data[:2], orchid_data[1:2]
 			if '%' == first_char:
 				counter = counter+1
@@ -21,20 +46,26 @@ class MainCompare:
 				counter+=1
 				next_data = data[counter]
 				result = []
+				print origin_data
 				while next_data != '//\n':
-					result.append(next_data[:next_data.index("/")])
+					result_data = self.convert_result(next_data)
+					result.append(result_data)
 					counter+=1
 					next_data = data[counter]
-# 				time_swath, correct_swath, wrong_swath = wordseg.swath(origin_data, result)
-# 				print time_swath, correct_swath, wrong_swath
-# 				time_wordcut, correct_wordcut, wrong_wordcut = wordseg.wordcut(origin_data, result)
-# 				print time_wordcut, correct_wordcut, wrong_wordcut
-				wordseg.thaisematic(origin_data, result)
-				break
+				time_swath, d_swath = wordseg.swath(origin_data, result)
+				time_wordcut, d_wordcut = wordseg.wordcut(origin_data, result)
+				time_sem, d_sem = wordseg.thaisematic(origin_data, result)
+				time_tlex, d_tlex = wordseg.Tlex(origin_data.decode('utf-8'), result)
+				time_libthai, d_libthai = wordseg.libthai(origin_data, result)
+				self.write_output_time(time_swath, time_wordcut, time_sem, time_tlex, time_libthai)
+				self.write_output_distance(d_swath, d_wordcut, d_sem, d_tlex, d_libthai)
 			counter = counter+1
 
-	def write_output(self, program_id, data_msg, exe_time, num_correct):
-		pass
+	def write_output_time(self, time_swath, time_wordcut, time_sem, time_tlex, time_libthai):
+		print time_swath, time_wordcut, time_sem, time_tlex, time_libthai
+	
+	def write_output_distance(self, d_swath, d_wordcut, d_sem, d_tlex, d_libthai):
+		print d_swath, d_wordcut, d_sem, d_tlex, d_libthai
 
 mainCmp = MainCompare()
 mainCmp.process_data()
--- wordsegmentation.py
+++ wordsegmentation.py
@@ -1,17 +1,42 @@
-import os, time, urllib2, sys, json
+# -*- coding: UTF-8 -*-
+import os, time, urllib2, json
+from suds.client import Client
+
+def levenshtein(a,b):
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a,b = b,a
+        n,m = m,n
+        
+    current = range(n+1)
+    for i in range(1,m+1):
+        previous, current = current, [i]+[0]*n
+        for j in range(1,n+1):
+            add, delete = previous[j]+1, current[j-1]+1
+            change = previous[j-1]
+            if a[j-1] != b[i-1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+            
+    return current[n]
 
 class WordSegmentation(object):
 
     def __init__(self):
         pass
         
-    def test(self):
-        print("test")
-        
     def write_file(self, msg):
         f = open('in.txt','w')
         f.write(msg)
         f.close()
+    
+    def write_file_th(self, msg):
+        f = open('thin.txt','w')
+        u = unicode(msg, "utf-8")
+        f.write(u.encode('tis-620'))
+        f.close()      
 
     def read_file(self):
         f = open('out.txt','r')
@@ -50,35 +75,89 @@ class WordSegmentation(object):
                    }
         headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "application/json"}
         params = json.dumps(request)
-# print params
         headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "application/json"}
         url = 'https://www.thaisemantics.org/service/rest/'
         req = urllib2.Request(url, params)
         req.add_header('Content-type','application/x-www-form-urlencoded')
         req.add_header('Accept','application/json')
+        start_time = time.time()
         u = urllib2.urlopen(req)
         data = u.read()
         json_data = json.loads(data)
+        total_time = time.time() - start_time #return in seconds
+        program_result = []
         for sep_data in json_data['result']:
-            print sep_data
-        print sep_data
+            program_result.append(sep_data)
+#         result_uni = [unicode(a, 'utf-8') for a in result]
+        distance = levenshtein([unicode(a, 'utf-8') for a in result], program_result)
+        return total_time, distance
 
     def wordcut(self, msg, result):
         return self.template_call(msg, result, "wordcut <in.txt> out.txt", ' ')
 
     def template_call(self, msg, result, command, separator):
-        trim_msg = msg.replace("//","")
+        trim_msg = msg.replace("//","").replace("\n","")
         self.write_file(trim_msg)
         start_time = time.time()
         os.system(command)
         total_time = time.time() - start_time #return in seconds
         out = self.read_file()
+        out = out.replace('\n','')
         out_lst = out.split(separator)
-        correct, wrong = self.count_answer(result, out_lst)
-        return total_time, correct, wrong
+        distance = levenshtein(result, out_lst)
+#         correct, wrong = self.count_answer(result, out_lst)
+        return total_time, distance
 
     def libthai(self, msg, result):
-        pass
+        trim_msg = self.befor_trim(msg)
+        self.write_file_th(trim_msg)
+        u = unicode("./libthai", "utf-8")
+        start_time = time.time()
+        os.system(u.encode('tis-620'))
+        total_time = time.time() - start_time #return in seconds
+        f = open('thout.txt','r')
+        str_out = f.read()
+        f.close()
+        str_out = str_out.decode('tis-620')
+        program_result = str_out.split('|')
+        uni_result = [unicode(a, 'utf-8') for a in result]
+        d = levenshtein(uni_result, program_result)
+        return total_time, d
 
     def Tlex(self, msg, result):
-        pass
+        client = Client('http://www.sansarn.com/WSeg/wsdl/BnSeg.wsdl')
+        msg = self.befor_trim(msg)
+        start_time = time.time()
+        out = client.service.seg(msg)
+        total_time = time.time() - start_time #return in seconds
+        str_out = self.after_trim(out)
+        str_out = str_out[:len(str_out)-1]
+        program_result = str_out.split('|')
+        uni_result = [unicode(a, 'utf-8') for a in result]
+        d = levenshtein(uni_result, program_result)
+        return total_time, d
+    
+    def befor_trim(self, msg):
+        return msg.replace("//","").replace("\n","")
+    
+    def after_trim(self, msg):
+        return msg.replace("\n","")
+
+# u = unicode('ทดสอบ','utf-8')
+# u1 = unicode('ทดสอบ','utf-8')
+# print u
+# print levenshtein([u,'b'], [u1,'b'])
+
+# wordseq = WordSegmentation()
+# wordseq.Tlex('test', [])
+# param = "ทดสอยได้ไหม"
+# u = unicode("./libthai "+param, "utf-8")
+# os.system(u.encode('tis-620'))
+# f = open('thaiout.txt','r')
+# msg = f.read()
+# print msg.decode('tis-620')
+# f.close()
+# f = open('test.txt','w')
+# f.write(u.encode('tis620'))
+# f.close()
+
