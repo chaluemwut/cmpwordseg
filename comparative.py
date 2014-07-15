e7af6714e23321eeb02fe80fc83de809bafee73a
chaluemwut_noyunsan <chaluemwut@gmail.com>
2014-07-15 23:07:15 +0700

    Time system test

--- comparative.py
+++ comparative.py
@@ -33,36 +33,67 @@ class MainCompare:
 
 		counter = 0
 		wordseg = WordSegmentation()
-		for orchid_data in data:
-			print orchid_data
-			first_char, sec_char, line_data = orchid_data[:1], orchid_data[:2], orchid_data[1:2]
+		for orchid_data in range(len(data)):
+			orchid_data = data[counter]
+			first_char, line_data = orchid_data[:1], orchid_data[1:2]
 			if '%' == first_char:
+				counter = counter+1
+				continue
+			
+			if first_char == '#' and line_data.isdigit() and data[counter+1][:1] == '%':
 				counter = counter+1
 				continue
 
 			if first_char == '#' and line_data.isdigit() :
-				counter+=1
+				counter = counter+1
 				origin_data = data[counter]
-				counter+=1
+				if origin_data[-2:] == '\\\n':
+					counter = counter+1
+					next_data = data[counter]
+					origin_data = origin_data + next_data
+					while next_data[-2:] == '\\\n':
+						counter = counter+1
+						next_data = data[counter]
+						origin_data = origin_data + next_data
+				counter = counter+1
+				print origin_data
 				next_data = data[counter]
 				result = []
-				print origin_data
 				while next_data != '//\n':
 					result_data = self.convert_result(next_data)
 					result.append(result_data)
-					counter+=1
+					counter = counter+1
 					next_data = data[counter]
-				time_swath, d_swath = wordseg.swath(origin_data, result)
-				time_wordcut, d_wordcut = wordseg.wordcut(origin_data, result)
-				time_sem, d_sem = wordseg.thaisematic(origin_data, result)
-				time_tlex, d_tlex = wordseg.Tlex(origin_data.decode('utf-8'), result)
-				time_libthai, d_libthai = wordseg.libthai(origin_data, result)
-				self.write_output_time(time_swath, time_wordcut, time_sem, time_tlex, time_libthai)
-				self.write_output_distance(d_swath, d_wordcut, d_sem, d_tlex, d_libthai)
+				
+# 				time_swath, d_swath = wordseg.swath(origin_data, result)
+# 				time_wordcut, d_wordcut = wordseg.wordcut(origin_data, result)
+# 				time_sem, d_sem = wordseg.thaisematic(origin_data, result)
+# 				time_tlex, d_tlex = wordseg.Tlex(origin_data.decode('utf-8'), result)
+# 				time_libthai, d_libthai = wordseg.libthai(origin_data, result)
+# 				self.write_output_time(time_libthai, time_swath, time_wordcut, time_sem, time_tlex)
+# 				self.write_output_distance(d_swath, d_wordcut, d_sem, d_tlex, d_libthai)
+									
+				try:					
+					time_swath, d_swath = wordseg.swath(origin_data, result)
+					time_wordcut, d_wordcut = wordseg.wordcut(origin_data, result)
+					time_sem, d_sem = wordseg.thaisematic(origin_data, result)
+					time_tlex, d_tlex = wordseg.Tlex(origin_data.decode('utf-8'), result)
+					time_libthai, d_libthai = wordseg.libthai(origin_data, result)
+					self.write_output_time(time_libthai, time_swath, time_wordcut, time_sem, time_tlex)
+					self.write_output_distance(d_swath, d_wordcut, d_sem, d_tlex, d_libthai)
+				except Exception:
+					print 'error'
+				
 			counter = counter+1
+			
+	def write_file(self, msg):
+		f = open('time.txt','a+b')
+		f.write(msg+'\n')
+		f.close()
 
-	def write_output_time(self, time_swath, time_wordcut, time_sem, time_tlex, time_libthai):
-		print time_swath, time_wordcut, time_sem, time_tlex, time_libthai
+	def write_output_time(self, time_libthai, time_swath, time_wordcut, time_sem, time_tlex):
+		msg = ','.join([str(time_libthai), str(time_swath), str(time_wordcut), str(time_sem), str(time_tlex)])
+		self.write_file(msg)
 	
 	def write_output_distance(self, d_swath, d_wordcut, d_sem, d_tlex, d_libthai):
 		print d_swath, d_wordcut, d_sem, d_tlex, d_libthai
